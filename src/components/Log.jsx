3951777d2905e13eb0c924e45fd8867049bbd706
GubernatorovKontur <workvmozg@gmail.com>
2025-04-24 22:51:03 +0300

    Create Log.jsx

---
+++ src/components/Log.jsx
@@ -0,0 +1,16 @@
+const { Button, Gapped, Loader, Textarea } = ReactUI;
+
+const Log = ({ logEntries, copyLog, clearLog, sendToGoogleSheets, loading }) => (
+  <Gapped vertical gap={20}>
+    <Gapped gap={10}>
+      <Button use="primary" onClick={copyLog}>Скопировать лог</Button>
+      <Button use="primary" onClick={clearLog}>Очистить лог</Button>
+      <Button use="primary" onClick={sendToGoogleSheets} disabled={loading}>
+        {loading ? <Loader /> : "Отправить в Google Таблицы"}
+      </Button>
+    </Gapped>
+    <Textarea value={logEntries.join("\n")} readOnly style={{ minHeight: '200px' }} />
+  </Gapped>
+);
+
+export default Log;
